6d999d4ecade39cbe65d7e546d0231d4ff4726f6
rafaelsousa <rafaelsousa@kislev>
2016-07-21 22:45:00 -0300

    I am still trying to port this application. Still lots of errors.

---
+++ src/main/java/com/github/rafaelsousa/fractalr2/lsystem/AcaoExpansiva.java
@@ -0,0 +1,12 @@
+package com.github.rafaelsousa.fractalr2.lsystem;
+
+/**
+ * @author rafael
+ */
+
+public class AcaoExpansiva extends AcaoLSystem {
+    
+    public AcaoLSystem acoesDerivadas;
+    
+        
+}
---
+++ src/main/java/com/github/rafaelsousa/fractalr2/lsystem/Ponto.java
@@ -0,0 +1,11 @@
+package com.github.rafaelsousa.fractalr2.lsystem;
+
+public class Ponto {
+	public Double x; 
+	public Double y;
+	
+	@Override
+	public String toString(){
+	    return "X:"+this.x+"Y:"+this.y;
+	} 
+}
