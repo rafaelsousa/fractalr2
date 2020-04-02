6dbc2555b10576d39492f9498edaf39abae7ed58
Rafael de Andrade Sousa <rafael.and@gmail.com>
2020-04-02 10:50:45 -0400

    Ported Pencil

--- src/main/java/ca/renardnumerique/fractalr2/Pencil.java
+++ src/main/java/ca/renardnumerique/fractalr2/Pencil.java
@@ -1,116 +1,137 @@
 package ca.renardnumerique.fractalr2;
 
-import javafx.animation.transition.*;
+import io.reactivex.rxjavafx.observables.JavaFxObservable;
+import javafx.animation.Animation;
+import javafx.animation.PathTransition;
 import javafx.scene.Group;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.PathElement;
+import javafx.util.Duration;
 
 import java.util.List;
 
 
-public class Pencil extends ImageView  {
-   private Boolean escreve=false;
-   private Group canvas;
-	private Integer contadorLinhas=0;
-	private List<Ponto> cood;
-	private Boolean mudouX = Boolean.FALSE;
-	private Boolean mudouY = Boolean.FALSE;
-	private DropShadow efeitoEscrita = new DropShadow();
-	{
-      efeitoEscrita.setRadius(20);
-      efeitoEscrita.setOffsetY(10);
-      efeitoEscrita.setOffsetX(-5);
-      efeitoEscrita.setSpread(.25);
-      efeitoEscrita.setColor(Color.web("#BDBD00" ));
-	};
-   private List<PathElement> linhas;
-   private Image imgPlay = new Image("imagens/play.png");
-   private Image imgPause = new Image("{__DIR__}imagens/pause.png");
+public class Pencil extends ImageView {
 
-   public var img= bind  if (animacao.running and not animacao.paused) imgPause else imgPlay;
-   override public var translateX on replace{
-	     //escreveRastro();
-	};
-   override public var translateY on replace{
-	     //escreveRastro();
-	};
-	override public var focusTraversable= true;
-	override public var  image=Image{url:"{__DIR__}imagens/lapis.png"};
 
-    var caminho = Path {
+    private static Pencil instance = new Pencil();
+
+
+    private Boolean escreve = false;
+    private Group canvas;
+    private Integer contadorLinhas = 0;
+    private List<Ponto> coordenates;
+    private Boolean mudouX = Boolean.FALSE;
+    private Boolean mudouY = Boolean.FALSE;
+
+    private DropShadow efeitoEscrita = new DropShadow();
+    {
+        efeitoEscrita.setRadius(20);
+        efeitoEscrita.setOffsetY(10);
+        efeitoEscrita.setOffsetX(-5);
+        efeitoEscrita.setSpread(.25);
+        efeitoEscrita.setColor(Color.web("#BDBD00"));
     }
 
-   public function escreveFractal(){
-       var inX=MainClass.instanciaAtual.design.areaDesenho.x+10;
-      var inY=MainClass.instanciaAtual.design.areaDesenho.y+25;
-       clear();
-  		var path = Path {
-              stroke: Color.web("#29345E")
-              strokeWidth: 1
-              elements: bind linhas
-         }
-  	   var ptAux=MoveTo { x: cood[0].x+inX   y: cood[0].y +inY};
-  	   insert path into canvas.content;
-	   insert ptAux into linhas;
-       for(pt in cood){
-           println("pt X:{ pt.x+inX  } Y:{ pt.y+inY }");
-            insert LineTo { x: pt.x+inX   y: pt.y+inY } into linhas
-       }
-
-   }
-   public function clear(){
-       contadorLinhas=0;
-       translateX=0;
-       translateY=0;
-       posicionaInicio();
-       delete linhas;
+    public Boolean focusTraversable = Boolean.TRUE;
+    public Image image = new Image("images/lapis.png");
 
 
-   }
-   public function processaCoordenadas(coordenadas:Ponto[]){
-       cood=coordenadas;
-       //delete linhas;
-       //delete caminho.elements;
-   }
-   protected function insere_caminho(coordX:Integer,coordY:Integer){
-	      insert LineTo { x:  coordX  y: coordY } into caminho.elements;
-   }
-   protected function posicionaInicio(){
-       //this.x=MainClass.instanciaAtual.design.areaDesenho.x+10;
-       //this.y=MainClass.instanciaAtual.design.areaDesenho.y-20;
-       //println("inicio x: {x} y: {y} translatex: {translateX} translatey: {translateY}");
-   }
-	public var animacao:PathTransition ;
+    private PathTransition animacao = new PathTransition();
 
-	public function stop(){
-	    lp.animacao.stop();
-	    posicionaInicio();
+    private List<PathElement> linhas;
+    private Image imgPlay = new Image("images/play.png");
+    private Image imgPause = new Image("images/pause.png");
 
-	}
-	public function play(){
-		var tempo =3+2*(Math.log(sizeof caminho.elements)/Math.log(2)); // log2 do numero de pontos;
-		///play(Duration.valueOf(tempo*1000));
-		constroi(Duration.valueOf(tempo*1000));
-	}
-	public function constroi(tempo:Duration){
-	    escreveFractal();
-	}
-}
-public static function reset():Lapis{
-    if(lp != null){
-        lp.animacao.stop();
-        lp.clear();
+    private ImageView playPauseButton = new ImageView(imgPlay);
+
+    private Pencil() {
+        JavaFxObservable
+                .valuesOf(animacao.statusProperty())
+                .map(this::setPencilImage)
+                .subscribe(playPauseButton::setImage);
     }
-    return get();
-}
-public static function get():Lapis{
-    if(lp == null){
-        lp = new Lapis();
+
+    private Image setPencilImage(Animation.Status status) {
+        switch (status) {
+            case RUNNING:
+                return imgPause;
+            default:
+                return imgPlay;
+        }
     }
-    return lp;
+
+    private Path caminho = new Path();
+
+    public void escreveFractal() {
+        var inX = MainClass.instance.design.areaDesenho.getX() + 10;
+        var inY = MainClass.instance.design.areaDesenho.getY() + 25;
+        clear();
+        Path path = new Path();
+        path.setStroke(Color.web("#29345E"));
+        path.setStrokeWidth(1);
+        path.getElements().addAll(linhas);
+
+        MoveTo ptAux = new MoveTo(coordenates.get(0).getX() + inX, coordenates.get(0).getY() + inY);
+
+        canvas.getChildren().addAll(path);
+        linhas.add(ptAux);
+        coordenates.stream().forEach(pt ->
+                {
+                    System.out.println(pt);
+                    linhas.add(new LineTo(pt.getX(), pt.getY()));
+                }
+        );
+    }
+
+    public void clear() {
+        contadorLinhas = 0;
+        this.setTranslateX(0);
+        this.setTranslateY(0);
+        posicionaInicio();
+        linhas.clear();
+    }
+
+    public void processaCoordenadas(List<Ponto> coordenadas) {
+        coordenates = coordenadas;
+    }
+
+    protected void insere_caminho(Integer coordX, Integer coordY) {
+        caminho.getElements().add(new LineTo(coordX, coordY));
+    }
+
+    protected void posicionaInicio() {
+        //this.x=MainClass.instanciaAtual.design.areaDesenho.x+10;
+        //this.y=MainClass.instanciaAtual.design.areaDesenho.y-20;
+        //println("inicio x: {x} y: {y} translatex: {translateX} translatey: {translateY}");
+    }
+
+
+    public void stop() {
+        this.animacao.stop();
+        posicionaInicio();
+    }
+
+    public void play() {
+        Double tempo = 3 + 2 * (Math.log(caminho.getElements().size()) / Math.log(2)); // log2 do numero de pontos;
+        constroi(new Duration(tempo * 1000));
+
+    }
+
+    public void constroi(Duration tempo) {
+        animacao.setDuration(tempo);
+        escreveFractal();
+    }
+
+    public void reset() {
+        this.animacao.stop();
+        this.clear();
+    }
+
 }
