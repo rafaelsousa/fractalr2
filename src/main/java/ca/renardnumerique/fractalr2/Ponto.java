a1b45b6aa3584e2f8d2e81db3129cff22736c32a
Rafael de Andrade Sousa <rafael.and@gmail.com>
2020-04-02 10:10:45 -0400

    Ported Ponto

--- src/main/java/ca/renardnumerique/fractalr2/Ponto.java
+++ src/main/java/ca/renardnumerique/fractalr2/Ponto.java
@@ -1,4 +1,29 @@
 package ca.renardnumerique.fractalr2;
 
+
 public class Ponto {
+
+    private Double x;
+    private Double y;
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "X:" + x + "Y:" + y;
+    }
 }
