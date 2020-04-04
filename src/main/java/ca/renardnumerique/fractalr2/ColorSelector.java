8578b0ff8e5eeee71350aa8946db3229d47f6e25
Rafael de Andrade Sousa <rafael.and@gmail.com>
2020-04-04 16:58:28 -0400

    Ported colorselector

--- src/main/java/ca/renardnumerique/fractalr2/ColorSelector.java
+++ src/main/java/ca/renardnumerique/fractalr2/ColorSelector.java
@@ -1,152 +1,151 @@
 package ca.renardnumerique.fractalr2;
 
 import javafx.scene.*;
-import javafx.scene.input.*;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
 import javafx.scene.text.*;
 
-public class SeletorCores extends Group  {
-    public var posX:Number;
-    public var posY:Number;
-    public var selecionado:Seletor;
-    public var idSelecionado=bind selecionado.idCor;
-    public var botaoPai:ActionButton;
-    public function setCor(id:Integer){ 
-        if(id ==0)
-        	selecionado = btnBranco;
-        if(id ==1)
-        	selecionado = btnVermelho;
-        if(id ==2)
-        	selecionado = btnVerde;
-        if(id ==3)
+public class ColorSelector extends Group  {
+    private Double posX;
+    private Double posY;
+    private Selector selecionado;
+    public Integer idSelecionado = selecionado.getIdCor();
+    private ActionButton botaoPai;
+
+    public ColorSelector(){
+        this.getChildren().addAll(
+                btnControle,
+                txtMais,
+                btnVermelho,
+                btnVerde,
+                btnAzul,
+                btnAmarelo,
+                btnBranco);
+    }
+
+    public void setCor(Integer id){
+        if(id == 0){
+            selecionado = btnBranco;
+        }
+        if(id == 1)
+        	{selecionado = btnVermelho;}
+        if(id == 2)
+        	{selecionado = btnVerde;}
+        if(id == 3)
         	selecionado = btnAzul;
-        if(id ==4)
+        if(id == 4)
         	selecionado = btnAmarelo;
         corGradiente();
     }
-    public var corGradiente= function(){ 
-          botaoPai.fillNormal=LinearGradient {
-                  startX: 0.0, startY: 0.7, endX: 0.0, endY: 1.5
-                  proportional: true
-                  stops: [ Stop { offset: 0.2 color:  selecionado.fill as Color },
-                           Stop { offset: 1.0 color: Color.web("#444444") }
-                          ]
-             }; 
+    public void corGradiente(){
+        botaoPai.setFillNormal(new LinearGradient(
+                        0.0,
+                        0.7,
+                        0.0,
+                        1.5,
+                        Boolean.TRUE,
+                        CycleMethod.NO_CYCLE,
+                        new Stop(0.0, Color.web("#c8eab2")),
+                        new Stop(1.0, Color.web("#aaa"))));
+
     }
     
-    public var nodo:Rectangle;
-	public var aberto=false;
-	var txtMais =Text {
-	      x: bind posX+2
-	       y:bind posY+10
-	       content: "+"
-	       font: Font { name: "Bitstream Vera Sans Bold", size: 10} 
-	       fill: Color.web("#021F77")
+    private Rectangle nodo;
+
+	public Boolean aberto = Boolean.FALSE;
+
+	private Text txtMais = new Text("+");
+    {
+	       txtMais.setX(posX + 2);
+	       txtMais.setY(posY + 10);
+	       txtMais.setFont(new Font ("Bitstream Vera Sans Bold", 10));
+	       txtMais.setFill(Color.web("#021F77"));
 	}
-    public var btnVermelho= Seletor{
-           idCor:1
-           x:bind posX+2;
-           nodo:bind nodo; 
-           y:bind posY
-           fill:Color.web("#D7AEFF");
-           visible:bind aberto; 
-           selecionado:function(cor:Seletor){
-               selecionado=cor;
-               corGradiente();
-           };
+
+    private Selector btnVermelho = new Selector();
+    {
+           btnVermelho.setIdCor(1);
+           btnVermelho.setX(posX+2);
+           btnVermelho.setNodo(nodo);
+           btnVermelho.setY(posY);
+           btnVermelho.setFill(Color.web("#D7AEFF"));
+           btnVermelho.setVisible(aberto);
     }
-    public var btnVerde= Seletor{
-			idCor:2
-           nodo:bind nodo; 
-           x:bind posX+14
-           y:bind posY
-           fill:Color.web("#2EFF9E"); 
-           visible:bind aberto; 
-           selecionado:function(cor:Seletor){
-               selecionado=cor;
-               corGradiente();
-           };
+    private Selector btnVerde = new Selector();
+    {
+    	   btnVerde.setIdCor(2);
+           btnVerde.setNodo(nodo);
+           btnVerde.setX(posX+14);
+           btnVerde.setY(posY);
+           btnVerde.setFill(Color.web("#2EFF9E"));
+           btnVerde.setVisible(aberto);
     }
-    public var btnAzul= Seletor{
-        idCor:3
-           nodo:bind nodo; 
-           x:bind posX+26
-           y:bind posY
-           fill:Color.web("#A7FFEC")
-           visible:bind aberto; 
-           selecionado:function(cor:Seletor){
-               selecionado=cor;
-               corGradiente();
-           };
+    private Selector btnAzul = new Selector();
+    {
+           btnAzul.setIdCor(3);
+           btnAzul.setNodo(nodo);
+           btnAzul.setX(posX+26);
+           btnAzul.setY(posY);
+           btnAzul.setFill(Color.web("#A7FFEC"));
+           btnAzul.setVisible(aberto);
     }
-    public var btnAmarelo= Seletor{
-           idCor:4
-           nodo:bind nodo; 
-           x:bind posX+38
-           y:bind posY
-           fill:Color.web("#FEFF8D"); 
-           visible:bind aberto; 
-           selecionado:function(cor:Seletor){
-               selecionado=cor;
-               corGradiente();
-           };
+
+    private Selector btnAmarelo = new Selector();
+    {
+           btnAmarelo.setIdCor(4);
+           btnAmarelo.setNodo(nodo);
+           btnAmarelo.setX(posX+38);
+           btnAmarelo.setY(posY);
+           btnAmarelo.setFill(Color.web("#FEFF8D"));
+           btnAmarelo.setVisible(aberto);
     }
-    public var btnBranco= Seletor{
-        	idCor:0
-           nodo:bind nodo; 
-           x:bind posX+50
-           y:bind posY
-           fill:Color.web("#ffffff"); 
-           visible:bind aberto; 
-           selecionado:function(cor:Seletor){
-               selecionado=cor;
-               corGradiente();
-           };
+    public Selector btnBranco = new Selector();
+    {
+       	   btnBranco.setIdCor(0);
+           btnBranco.setNodo(nodo);
+           btnBranco.setX(posX+50);
+           btnBranco.setY(posY);
+           btnBranco.setFill(Color.web("#ffffff"));
+           btnBranco.setVisible(aberto);
     }
-    public var btnControle= Rectangle{
-           x:bind posX 
-           y:bind posY
-           width: 12 
-           height: 12
-           arcWidth: 8  
-           arcHeight: 8
-           cursor:Cursor.HAND
-       	   fill :Color.TRANSPARENT;
-       	    
-           stroke:Color.web("#888")
-           public var onMouseEntered= function(e:MouseEvent):Void{
-    	       stroke=Color.web("#444444");
-           }
-           public var onMouseExited= function(e:MouseEvent):Void{
-    	       stroke=Color.web("#888");
-	           width =12; 
-	           if(aberto)
-	           	posX=posX+48;
-	           aberto=false;
-	           txtMais.content="+";
-	           cursor=Cursor.HAND;
-           }
-           public var onMouseClicked= function(e:MouseEvent):Void{
-    	       stroke=Color.web("#1289FF");
-	           width =60;
-	           if(not aberto) 
-	           	posX=posX-48;
-	           aberto=true;
-	           cursor=Cursor.DEFAULT;
-	           txtMais.content="";
-           }
+
+    public Rectangle btnControle = new Rectangle();
+    {
+           btnControle.setX(posX);
+           btnControle.setY(posY);
+           btnControle.setWidth(12);
+           btnControle.setHeight(12);
+           btnControle.setArcWidth(8);
+           btnControle.setArcHeight( 8);
+           btnControle.setCursor(Cursor.HAND);
+       	   btnControle.setFill(Color.TRANSPARENT);
+           btnControle.setStroke(Color.web("#888"));
+           btnControle.setOnMouseEntered(e->{
+    	       btnControle.setStroke(Color.web("#444444"));
+           });
+           btnControle.setOnMouseExited(e->{
+    	       btnControle.setStroke(Color.web("#888"));
+	           btnControle.setWidth(12);
+	           if(aberto){
+	               posX = posX+48;
+	           }
+	           aberto = false;
+	           txtMais.setText("+");
+	           txtMais.setCursor(Cursor.HAND);
+           });
+
+           btnControle.setOnMouseClicked(e->{
+    	       btnControle.setStroke(Color.web("#1289FF"));
+	           btnControle.setWidth(60);
+	           if(!aberto){
+	               posX = posX-48;
+	           }
+	           aberto = true;
+	           btnControle.setCursor(Cursor.DEFAULT);
+	           txtMais.setText("");
+           });
        };
-    
-    public function create(): Node {
-           Group{
-           content:[
-	           btnControle,
-			   txtMais,
-			   btnVermelho,btnVerde,btnAzul,btnAmarelo,btnBranco
-			 ]
-           }
-    }
+
 }
 
  
