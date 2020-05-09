1003d9f677e290eda52d58eee8a4841a2ddd5b4b
Rafael de Andrade Sousa <rafael.and@gmail.com>
2020-05-09 08:21:00 -0400

    Refactoring

--- src/main/java/ca/renardnumerique/fractalr2/Start.java
+++ src/main/java/ca/renardnumerique/fractalr2/Start.java
@@ -1,8 +1,7 @@
 package ca.renardnumerique.fractalr2;
 
+import ca.renardnumerique.fractalr2.utils.Constants;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Start extends Application {
@@ -11,8 +10,8 @@ public class Start extends Application {
     public void start(Stage stage) {
         stage.setTitle("Fractal-R Version 1.9 - Beta");
         stage.setResizable(Boolean.FALSE);
-        stage.setWidth(MainClass.getInstance().getSystemWidth());
-        stage.setHeight(MainClass.getInstance().getSystemHeight());
+        stage.setWidth(Constants.SYSTEM_WIDTH);
+        stage.setHeight(Constants.SYSTEM_HEIGHT);
         MainClass.getInstance().create(stage);
         stage.show();
     }
